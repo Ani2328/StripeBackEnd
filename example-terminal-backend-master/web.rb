db93c2c621751f5cb8d36a0ac5a93297d7559322
Anitha Jasmine <anitha.gnanaprakasam@ascendion.com>
2024-09-13 11:37:39 +0530

    key updated

--- example-terminal-backend-master/web.rb
+++ example-terminal-backend-master/web.rb
@@ -27,6 +27,7 @@ end
 
 Dotenv.load
 Stripe.api_key = ENV['STRIPE_ENV'] == 'production' ? ENV['STRIPE_SECRET_KEY'] : ENV['STRIPE_TEST_SECRET_KEY']
+puts "===="+Stripe.api_key
 Stripe.api_version = '2020-03-02'
 
 def log_info(message)
